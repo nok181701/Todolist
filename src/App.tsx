93b946c340e257ab69cf1c4b232b4ff46d24d95b
祝井直樹 <nok181701@gmail.com>
2023-04-19 14:21:27 +0900

    ✨ To doリスト

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { log } from "console";
 
 function App() {
+  //①todo各項目の文字列の値、②todo各項目をそれぞれ認識するためのid、③完了or未完了の識別をする識別、が必要。⇨型指定する。
+  type Todo = {
+    inputvalue: string;
+    id: number;
+    checked: boolean;
+  };
+
+  const [inputvalue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+  const hundleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    //新しいTodoの作成
+    const newTodo: Todo = {
+      inputvalue: inputvalue,
+      id: todos.length,
+      checked: false,
+    };
+    setTodos([newTodo, ...todos]);
+    setInputValue("");
+  };
+
+  const handleEdit = (id: number, inputvalue: string) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.inputvalue = inputvalue;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const handlechecked = (id: number, checked: boolean) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.checked = !checked;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const handleDelete = (id: number) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <h2>Todoリスト with TypeScript</h2>
+        <form
+          onSubmit={(e) => {
+            hundleSubmit(e);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <input type="text" onChange={handleChange} className="inputText" />
+          <input type="submit" className="submitButton " value="作成" />
+        </form>
+        <ul className="todoList">
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input
+                type="text"
+                onChange={(e) => handleEdit(todo.id, e.target.value)}
+                className="inputText"
+                value={todo.inputvalue}
+                disabled={todo.checked}
+              />
+              <input
+                type="checkbox"
+                onChange={(e) => handlechecked(todo.id, todo.checked)}
+              />
+              <button onClick={(e) => handleDelete(todo.id)}>消す</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
